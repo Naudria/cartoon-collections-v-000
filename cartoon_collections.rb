7b55ee6d0108d8581dd1b09e3056ca55f5bbcacd
Rima Jean <rima.jean@gmail.com>
2018-02-20 19:46:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)
   # Your code here
   dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
   dwarves.each_with_index do |name , index|
-    puts "(#{index} + 1) #{name}"
+    puts "#{index} + 1 #{name}"
   end
 end
 

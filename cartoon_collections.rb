d720fed63e95db8d2abc0e05e8a35aad3b793fba
Rima Jean <rima.jean@gmail.com>
2018-02-20 19:59:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,10 +15,10 @@ end
 
 def long_planeteer_calls(long_planeteer_calls)# code an argument here
   # Your code here
+  answer = false
   long_planeteer_calls.each do |call|
   if call.length > 4
     answer = true
-  else false
   end
 end
 answer

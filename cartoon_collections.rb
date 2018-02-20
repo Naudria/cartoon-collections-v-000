5ba498c1674e9042e3f12f010ee8571f58fdfb90
Rima Jean <rima.jean@gmail.com>
2018-02-20 19:56:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,13 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_planeteer_calls)# code an argument here
   # Your code here
+  long_planeteer_calls.each do |call|
+  if call.length > 4
+    true
+  end
+    
 end
 
 def find_the_cheese# code an argument here
